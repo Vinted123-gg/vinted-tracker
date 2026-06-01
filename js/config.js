cb2f2f9f51550a2e2b211ab7025d6a360a54f138
Vinted123-gg <vinted.provic@gmail.com>
2026-06-01 21:04:30 +0200

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,22 @@
+const CONFIG = {
+  GOOGLE_CLIENT_ID: '1086306113674-g0pq4rv702aqrvol7hopifkhnssrr69b.apps.googleusercontent.com',
+  GOOGLE_SCOPES: 'https://www.googleapis.com/auth/gmail.readonly',
+  VINTED_SENDERS: [
+    'no-reply@vinted.fr',
+    'noreply@vinted.fr',
+    'no-reply@vinted.be',
+    'no-reply@vinted.lu',
+    'transaction@vinted.fr'
+  ],
+  DEFAULT_COMMISSION: 5,
+  ACCOUNT_COLORS: [
+    '#009EE0',
+    '#1D9E75',
+    '#D85A30',
+    '#7F77DD',
+    '#D4537E',
+    '#BA7517',
+    '#185FA5',
+    '#3B6D11',
+  ],
+};

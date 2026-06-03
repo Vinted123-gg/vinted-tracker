0bc7ca7afabfc1657b7eaf63ac9c251793cc83b6
Vinted123-gg <vinted.provic@gmail.com>
2026-06-03 22:45:43 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,8 @@
 const CONFIG = {
   GOOGLE_CLIENT_ID: '1086306113674-g0pq4rv702aqrvol7hopifkhnssrr69b.apps.googleusercontent.com',
   GOOGLE_SCOPES: 'https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
+  SUPABASE_URL: 'https://vimdhtgpiiucpnrzvoef.supabase.co',
+  SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpbWRodGdwaWl1Y3Bucnp2b2VmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1MDk2MDYsImV4cCI6MjA5NjA4NTYwNn0.cyIvtWo5MFf9blvRbbWPIWenk_dqtfWJsGMB231h5aQ',
   VINTED_SENDERS: [
     'no-reply@vinted.fr',
     'noreply@vinted.fr',
